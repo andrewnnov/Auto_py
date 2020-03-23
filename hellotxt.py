2fa763c91c7b5e1df1cc6447cad932d82b596d95
andreybannikov <andrewnnov@yandex.ru>
2020-03-23 23:17:34 +0300

    open and read file

---
+++ hellotxt.py
@@ -0,0 +1,14 @@
+import os
+
+helloFile = open('C:\\java\\pythonfolder\\hello.txt')
+
+helloContent = helloFile.read()
+print(helloContent)
+
+
+helloFile1 = open('C:\\java\\pythonfolder\\hello.txt')
+print(helloFile1.readlines())
+
+
+
+
