dcbdfc86fb18d844b0dd52e4334f4d89a0ef7288
andreybannikov <andrewnnov@yandex.ru>
2020-03-27 20:51:12 +0300

    move file

---
+++ shutilmoving.py
@@ -0,0 +1,6 @@
+import shutil
+
+shutil.move('C:\\java\\ex\\moving.txt', 'C:\\java\\ex\\delicious')
+#shutil.move('C:\\java\\ex\\moving.txt', 'C:\\java\\ex\\mov')
+
+shutil.move('C:\\java\\ex\\moving1.txt', 'C:\\java\\ex\\delicious')
