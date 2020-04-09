60bed98120c1a5162e5638ff52dcbc3e0309a29f
andreybannikov <andrewnnov@yandex.ru>
2020-04-09 22:15:39 +0300

    copy and move file

---
+++ copyfile.py
@@ -0,0 +1,9 @@
+import shutil, os
+
+#shutil.copy('C:\\python\\pp.txt', 'C:\\python\\past')
+
+
+#shutil.copytree('C:\\python\\copy', 'C:\\python\\copy_backup')
+
+
+shutil.move('C:\\python\\pp.txt', 'C:\\python\\past\\new_bacon.txt')
